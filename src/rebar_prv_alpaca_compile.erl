4496882a0b0fd60b2c9b86382c7db138a7ea2129
Jeremy Pierre <jeremy@Jeremys-MacBook-Pro.local>
2016-12-07 16:22:31 -0800

    Support for renaming from MLFE to Alpaca

--- src/rebar_prv_alpaca_compile.erl
+++ src/rebar_prv_alpaca_compile.erl
@@ -1,9 +1,9 @@
--module(rebar_prv_mlfe_compile).
+-module(rebar_prv_alpaca_compile).
 
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, compile).
--define(NAMESPACE, mlfe).
+-define(NAMESPACE, alpaca).
 -define(DEPS, [{default, app_discovery}]).
 
 %% ===================================================================
@@ -17,9 +17,9 @@ init(State) ->
             {module, ?MODULE},            % The module implementation of the task
             {bare, true},                 % The task can be run by the user, always true
             {deps, ?DEPS},                % The list of dependencies
-            {example, "rebar3 mlfe compile"}, % How to use the plugin
+            {example, "rebar3 alpaca compile"}, % How to use the plugin
             {opts, []},                   % list of options understood by the plugin
-            {short_desc, "mlfe rebar3 compiler plugin"},
+            {short_desc, "Alpaca rebar3 compiler plugin"},
             {desc, ""}
     ]),
     {ok, rebar_state:add_provider(State, Provider)}.
@@ -38,10 +38,10 @@ do(State) ->
          %% Opts = rebar_app_info:opts(AppInfo),
          EBinDir = rebar_app_info:ebin_dir(AppInfo),
          SourceDir = filename:join(rebar_app_info:dir(AppInfo), "src"),
-         FoundFiles = rebar_utils:find_files(SourceDir, ".*\\.mlfe\$"),
+         FoundFiles = rebar_utils:find_files(SourceDir, ".*\\.alp\$"),
 
          [file:write_file(filename:join(EBinDir, FileName), BeamBinary) ||
-             {compiled_module, ModuleName, FileName, BeamBinary} <- mlfe:compile({files, FoundFiles}, TestsEnabled)]
+             {compiled_module, ModuleName, FileName, BeamBinary} <- alpaca:compile({files, FoundFiles}, TestsEnabled)]
      end || AppInfo <- Apps],
 
     {ok, State}.
